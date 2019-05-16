f11cb101ad503c3eeb999a8c90b7cc9861a502cc
FalseCrystal <1706969@wits.students.ac.za>
2019-05-16 12:04:01 +0200

    Final Hand in

--- unitTest1.py
+++ unitTest1.py
@@ -10,6 +10,11 @@ class TestExclude(unittest.TestCase):
            assertEqual(testDict,[[Levitt,ELEN4010],[Hazelhurst,ElEN7039],[Cronje,ELEN4014],[Dinger,SCMD1003],[Hazelhurst,ELEN3020],[Hazelhurst,COMS7059],[Hofsajer,ELEN3003],[Levitt,ELEN3008],[Hofsajer,ELEN3002]])
            f.close()
 
+      def test_getCours():
+           f = open("lectlist.csv")
+           testline = getCourse(f[0])
+           assertEqual(testline,"Levitt")
+
 if __name__ == '__main__':
 
      unittest.main() 
